36a27bd744a653894b43d95c712496d6ef455c64
guanbinrui <guanbinrui@dimension.im>
2021-04-10 08:30:29 +0800

    chore: hide internal plugins

--- packages/maskbook/src/plugins/PluginUI.ts
+++ packages/maskbook/src/plugins/PluginUI.ts
@@ -15,7 +15,7 @@ import { FileServicePluginDefine } from './FileService/UI-define'
 import { TraderPluginDefine } from './Trader/define'
 import { TransakPluginDefine } from './Transak/define'
 import { ITO_PluginDefine } from './ITO/define'
-import { NFTPluginsDefine } from './NFT/define'
+import { NFT_PluginsDefine } from './NFT/define'
 import { AirdropPluginDefine } from './Airdrop/define'
 import { sideEffect } from '../utils/side-effects'
 
@@ -25,7 +25,7 @@ sideEffect.then(() => {
     plugins.add(RedPacketPluginDefine)
     plugins.add(FileServicePluginDefine)
     plugins.add(ITO_PluginDefine)
-    plugins.add(NFTPluginsDefine)
+    plugins.add(NFT_PluginsDefine)
     if (Flags.gitcoin_enabled) plugins.add(GitcoinPluginDefine)
     if (Flags.poll_enabled) plugins.add(PollsPluginDefine)
     if (Flags.trader_enabled) plugins.add(TraderPluginDefine)
